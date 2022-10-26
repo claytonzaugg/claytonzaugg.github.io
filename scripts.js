0d57804c30ced114d951eabe1eb3a0793293e48b
claytonzaugg <clayton.zaugg@gmail.com>
2022-10-26 00:14:36 -0600

    initial commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function expand(event){
+    const toExpand = document.querySelector('article');
+
+    toExpand.classList.toggle('active');
+}
+
+function close(event) {
+    const toExpand = document.querySelector('article');
+
+    toExpand.classList.toggle('active');
+}
