25349104d7e525022e94bd384f8c8998f9e1c2e6
claytonzaugg <clayton.zaugg@gmail.com>
2022-10-26 01:39:22 -0600

    Everything but mobile ready

--- scripts.js
+++ scripts.js
@@ -1,11 +1,7 @@
 function expand(event){
-    const toExpand = document.querySelector('article');
+    const articleAction = document.querySelector('article');
+    const exploreAction = document.querySelector('.explore-more-details');
 
-    toExpand.classList.toggle('active');
-}
-
-function close(event) {
-    const toExpand = document.querySelector('article');
-
-    toExpand.classList.toggle('active');
+    articleAction.classList.toggle('active');
+    exploreAction.classList.toggle('active');
 }
